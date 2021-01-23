f75b3a094b38340ab5f172ca044510118392481c
“Akshaya-15” <akshaya.adisht90@gmail.com>
2021-01-23 10:57:55 +0530

    Added:Valid Last Name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -5,49 +5,52 @@ import java.util.regex.*;
 
 public class UserRegistration {
 	
-// FirstName method
+// FirstName 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
-		
+
 		System.out.println("Enter First Name:");
-		Scanner scanner=new Scanner(System.in);
-		String userInput=scanner.next();
-		
-		Pattern firstNameRegExp=Pattern.compile("(^[A-Z]{1})[a-z]{2,}");
-		Matcher matchResult=firstNameRegExp.matcher(userInput);
-		boolean firstNameResult=matchResult.matches();
-		if(firstNameResult == true)
-		{
+		Scanner scanner = new Scanner(System.in);
+		String userInput = scanner.next();
+
+		Pattern firstNameRegExp = Pattern.compile("(^[A-Z]{1})[a-z]{2,}");
+		Matcher matchResult = firstNameRegExp.matcher(userInput);
+		boolean firstNameResult = matchResult.matches();
+		if (firstNameResult == true) {
+			System.out.println(firstNameResult);
+		} else {
 			System.out.println(firstNameResult);
 		}
-		else
-		{
-			System.out.println(firstNameResult);
+
+		// Last Name
+		System.out.println("Enter Last Name:");
+		Scanner scanner1 = new Scanner(System.in);
+		String userInputLastName = scanner1.next();
+
+		Pattern lastNameRegExp = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+		Matcher matchResultLastName = lastNameRegExp.matcher(userInputLastName);
+		boolean lastNameResult = matchResultLastName.matches();
+		if (lastNameResult == true) {
+			System.out.println(lastNameResult);
+		} else {
+			System.out.println(lastNameResult);
 		}
-	
-	
+// Email 
+		System.out.println("Enter Email:");
+		Scanner scanner2 = new Scanner(System.in);
+		String userInputemail = scanner2.next();
 
-
-    System.out.println("Enter Last Name:");
-    Scanner scanner1=new Scanner(System.in);
-	String userInputLastName=scanner1.next();
-
-	Pattern lastNameRegExp=Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-	Matcher matchResultLastName=lastNameRegExp.matcher(userInputLastName);
-	boolean lastNameResult=matchResultLastName.matches();
-	if(lastNameResult == true)
-	{
-		System.out.println(lastNameResult);
-	}
-	else
-	{
-		System.out.println(lastNameResult);
+		Pattern emailRegExp = Pattern.compile("^[a-zA-Z]{3}\\.[a-zA-Z]{1}@[a-zA-Z]{2}\\.([a-zA-Z]{2}\\.[a-zA-Z]{1,}$");
+		Matcher matchResultemail = emailRegExp.matcher(userInputemail);
+		boolean emailResult = matchResultemail.matches();
+		if (emailResult == true) {
+			System.out.println(emailResult);
+		} else {
+			System.out.println(emailResult);
+		}
 	}
 
 }
-
-}
-	
 	
 
 
--- src/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -16,11 +16,19 @@ public class UserRegistrationTest {
 	@Test
 	public static boolean LastNameValidation(String userInputLastName)
 	{
-		Pattern lastNameRegExp=Pattern.compile("([A-Z]{1}[a-z]{2,}(\\s{1}){0,}[A-Z]{1}[a-z]{2,})");
+		Pattern lastNameRegExp=Pattern.compile("^[A-Z]{1}[a-z]{2,}(\\s{1}){0,}[A-Z]{1}[a-z]{2,}$");
 		Matcher matchResult=lastNameRegExp.matcher(userInputLastName);
 		boolean lastNameResult=matchResult.matches();
 		return lastNameResult;
 	}
+	@Test
+	public static boolean EmailValidation(String userInputEmail)
+	{
+		Pattern emailRegExp=Pattern.compile("^[a-zA-Z]{3}\\.[a-zA-Z]{1}@[a-zA-Z]{2}\\.([a-zA-Z]{2}\\.[a-zA-Z]{1,}$");
+		Matcher matchResult=emailRegExp.matcher(userInputEmail);
+		boolean lastNameResult=matchResult.matches();
+		return lastNameResult;
+	
 
 }
 
